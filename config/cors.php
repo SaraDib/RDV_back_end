502e36bfbf4fc99c4344b63ce803f7906ec0737a
Dib Sara <dibsara270@gmail.com>
2026-02-21 21:32:57 +0000

    cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,12 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://booking.rakopssolutions.com'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://booking.rakopssolutions.com',
+        'https://www.booking.rakopssolutions.com'
+    ],
     'allowed_headers' => ['*'],
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
